06a22685f29a1d65bd246091bd87e9e63fe7d6b9
Thanh Minh Le <75014161+itstarsec@users.noreply.github.com>
2025-03-06 14:12:19 +0700

    Update server_telegram_chatbot.py

--- server_telegram_chatbot.py
+++ server_telegram_chatbot.py
@@ -1,5 +1,5 @@
 import asyncio
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import threading
@@ -16,6 +16,20 @@ app_flask = Flask(__name__)
 @app_flask.route('/status-check', methods=['GET'])
 def status_check():
     return jsonify({"status": last_status})
+
+@app_flask.route('/update-status', methods=['POST'])
+def update_status():
+    global last_status
+    data = request.get_json()
+    
+    if 'status' in data:
+        new_status = data['status']
+        if new_status in ['yes', 'no']:
+            last_status = new_status
+            return jsonify({"message": "Trạng thái đã được cập nhật", "new_status": last_status}), 200
+        else:
+            return jsonify({"message": "Trạng thái không hợp lệ, vui lòng gửi 'yes' hoặc 'no'"}), 400
+    return jsonify({"message": "Thiếu tham số 'status'"}), 400
 
 # Bot Telegram
 async def yes(update: Update, context: ContextTypes.DEFAULT_TYPE):
